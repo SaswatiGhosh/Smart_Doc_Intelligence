5ee13d329d82f83c6891702a8d874178051d81d2
4RN4B <arnabjuniormondal123@gmail.com>
2025-06-02 07:42:25 +0530

    Changed the gemini model to recent one

--- myproject_doc_intelligence/myproject_doc_intelligence/utils/sns_invoking_summary.py
+++ myproject_doc_intelligence/myproject_doc_intelligence/utils/sns_invoking_summary.py
@@ -2,12 +2,12 @@ import boto3
 import json
 import time
 from django.conf import settings
-import google.generativeai as genai
+from google import genai
 import os
 
 
 def sns_invoking_summary():
-    genai.configure(api_key=settings.GCP_API_KEY)
+    client = genai.Client(api_key=settings.GCP_API_KEY)
     dynamodb = boto3.resource("dynamodb")
     table = dynamodb.Table("Database_storage_for_doc_analysis")
     story = []
@@ -19,10 +19,12 @@ def sns_invoking_summary():
             story = response["Item"]["lines"]
             break
         time.sleep(2)
-    model = genai.GenerativeModel("gemini-1.5-flash")
-    model_response = model.generate_content(
-        [f"Please summarize the following text which is provided as lines:\n\n{story}"]
+
+    model_response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents=f"Please summarize the following text which is provided as lines:\n\n{story}",
     )
+    print(model_response.text)
     response = table.put_item(
         Item={
             "user_id": "anonymous",
